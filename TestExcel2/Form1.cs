c01da80b5825dc385cf5df0f5bdacda1b68efa25
rilwing <rilwing@rilwing.ru>
2025-04-14 19:34:20 +0700

    init

---
+++ TestExcel2/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace TestExcel2
+{
+    public partial class Form1 : Form
+    {
+        private string path = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            path = openFileDialog1.FileName;
+            MessageBox.Show("Файл открыт");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (path.Length == 0)
+            {
+                MessageBox.Show("Откройте файл");
+                return;
+            }
+
+            try
+            {
+                //Create COM Objects. Create a COM object for everything that is referenced
+                Excel.Application xlApp = new Excel.Application();
+                Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(path);
+                Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+                Excel.Range xlRange = xlWorksheet.UsedRange;
+
+                // Получаем ссылку на фигуру
+                Excel.Shape shape = xlWorksheet.Shapes.Item(1);
+                // Добавляем новую фигуру того же типа с той же шириной и высотой по указанным координатам
+                xlWorksheet.Shapes.AddShape(shape.AutoShapeType, 10, 10, shape.Width, shape.Height);
+
+                xlWorkbook.Save();
+                //cleanup
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
+                //rule of thumb for releasing com objects:
+                //  never use two dots, all COM objects must be referenced and released individually
+                //  ex: [somthing].[something].[something] is bad
+
+                //release com objects to fully kill excel process from running in the background
+                Marshal.ReleaseComObject(xlRange);
+                Marshal.ReleaseComObject(xlWorksheet);
+
+                //close and release
+                xlWorkbook.Close();
+                Marshal.ReleaseComObject(xlWorkbook);
+
+                //quit and release
+                xlApp.Quit();
+                Marshal.ReleaseComObject(xlApp);
+
+                MessageBox.Show("Готово");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
